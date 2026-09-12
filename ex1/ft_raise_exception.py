cf5d9c39b1b0513813b680a4fc30fc09c170c106
LucasBods <lucasferreira.bods@gmail.com>
2026-09-11 23:53:35 -0300

    ex0 ex1 ok

--- ex1/ft_raise_exception.py
+++ ex1/ft_raise_exception.py
@@ -2,6 +2,10 @@
 
 def input_temperature(temp_str: str) -> int:
     temp_int = int(temp_str)
+    if temp_int < 0:
+        raise ValueError(f"{temp_int}°C is too cold for plants (min 0°C)")
+    if temp_int > 40:
+        raise ValueError(f"{temp_int}°C is too hot for plants (max 40°C)")
     return (temp_int)
 
 
@@ -25,10 +29,30 @@ def test_temperature() -> None:
     except ValueError as e:
         print(f"Caught input_temperature error: {e}")
     finally:
+        print("")
+
+    try:
+        x = "100"
+        print(f"Input data is '{x}'")
+        x = input_temperature(x)
+        print(f"Temperature is now {x}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    finally:
+        print("")
+
+    try:
+        x = "-50"
+        print(f"Input data is '{x}'")
+        x = input_temperature(x)
+        print(f"Temperature is now {x}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    finally:
         print("\nAll tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
 
-    print("=== Garden Temperature ===\n")
+    print("=== Garden Temperature Checker ===\n")
     test_temperature()
